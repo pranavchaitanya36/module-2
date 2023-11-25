74e0cb26790b5dafab0ea29a14b21db8757541d7
pranavchaitanya36 <150655214+pranavchaitanya36@users.noreply.github.com>
2023-11-25 11:27:55 +0530

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,60 +1,85 @@
+
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
-
-//import "hardhat/console.sol";
 
 contract Assessment {
     address payable public owner;
     uint256 public balance;
 
-    event Deposit(uint256 amount);
-    event Withdraw(uint256 amount);
+    event Deposit(address indexed depositor, uint256 newAmount, string notification);
+    event Withdraw(address indexed withdrawer, uint256 newAmount, string notification);
+    event ChangeOwner(address indexed newerOwner);
+    event PurchaseItem(address indexed buyer, string newItem, uint256 newAmount, string notification);
+    event RewardUser(address indexed receiver, uint256 newAmount, string notification);
 
     constructor(uint initBalance) payable {
         owner = payable(msg.sender);
         balance = initBalance;
     }
 
-    function getBalance() public view returns(uint256){
+    function getBalance() public view returns (uint256) {
         return balance;
     }
 
-    function deposit(uint256 _amount) public payable {
-        uint _previousBalance = balance;
+    function deposit(uint256 depositAmount) public payable {
+        uint256 previousBalance = balance;
 
         // make sure this is the owner
         require(msg.sender == owner, "You are not the owner of this account");
 
         // perform transaction
-        balance += _amount;
+        balance += depositAmount;
 
         // assert transaction completed successfully
-        assert(balance == _previousBalance + _amount);
+        assert(balance == previousBalance + depositAmount);
 
-        // emit the event
-        emit Deposit(_amount);
+        // emit the event with notification
+        emit Deposit(msg.sender, depositAmount, string(abi.encodePacked("You deposited ", depositAmount, " ETH")));
     }
 
     // custom error
-    error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
+    error InsufficientBalance(uint256 currentBalance, uint256 withdrawalAmount);
 
-    function withdraw(uint256 _withdrawAmount) public {
+    function withdraw(uint256 withdrawalAmount) public {
         require(msg.sender == owner, "You are not the owner of this account");
-        uint _previousBalance = balance;
-        if (balance < _withdrawAmount) {
+        uint256 previousBalance = balance;
+        if (balance < withdrawalAmount) {
             revert InsufficientBalance({
-                balance: balance,
-                withdrawAmount: _withdrawAmount
+                currentBalance: balance,
+                withdrawalAmount: withdrawalAmount
             });
         }
 
         // withdraw the given amount
-        balance -= _withdrawAmount;
+        balance -= withdrawalAmount;
 
         // assert the balance is correct
-        assert(balance == (_previousBalance - _withdrawAmount));
+        assert(balance == (previousBalance - withdrawalAmount));
 
-        // emit the event
-        emit Withdraw(_withdrawAmount);
+        // emit the event with notification
+        emit Withdraw(msg.sender, withdrawalAmount, string(abi.encodePacked("You withdrew ", withdrawalAmount, " ETH")));
+    }
+
+    function changeOwner(address payable newerOwner) public {
+        require(msg.sender == owner, "You are not the owner of this account");
+        owner = newerOwner;
+        emit ChangeOwner(newerOwner);
+    }
+
+    function purchaseItem(string memory newItem, uint256 purchaseAmount) public {
+        require(msg.sender == owner, "You are not the owner of this account");
+        require(balance >= purchaseAmount, "Insufficient funds for purchase");
+        balance -= purchaseAmount;
+
+        // emit the event with notification
+        emit PurchaseItem(msg.sender, newItem, purchaseAmount, string(abi.encodePacked("You purchased ", newItem)));
+    }
+
+    function rewardUser(address receiver, uint256 rewardAmount) public {
+        require(msg.sender == owner, "You are not the owner of this account");
+        balance -= rewardAmount;
+
+        // emit the event with notification
+        emit RewardUser(receiver, rewardAmount, string(abi.encodePacked("You received ", rewardAmount, " ETH as a reward")));
     }
 }
